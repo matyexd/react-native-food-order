14c71673aa4f7b07d456f12c2d9a692502232ed6
Matvey <matv.softgreed@gmail.com>
2022-05-01 14:53:43 +0700

    finished dropdown

--- src/components/ui-kit/UiDropdown/UiDropdown/UiDropdown.jsx
+++ src/components/ui-kit/UiDropdown/UiDropdown/UiDropdown.jsx
@@ -3,13 +3,15 @@ import SelectDropdown from 'react-native-select-dropdown';
 // import {View} from 'react-native'
 import {fonts} from '../../../../theme/Fonts';
 import {height} from '../../../../utils/Responsive';
+import UiIcon from '../../UiIcon';
 
-const UiDropdown = ({titleDropdown = 'Select'}) => {
-  const countries = ['Egypt', 'Canada', 'Australia', 'Ireland'];
-
+const UiDropdown = ({titleDropdown = 'Select', categories}) => {
   return (
     <SelectDropdown
-      data={countries}
+      data={categories}
+      onSelect={(selectedItem, index) => {
+        console.log(selectedItem);
+      }}
       buttonTextAfterSelection={(selectedItem, index) => {
         return selectedItem;
       }}
@@ -23,9 +25,17 @@ const UiDropdown = ({titleDropdown = 'Select'}) => {
         borderRadius: height(8),
         backgroundColor: 'white',
         height: height(50),
+        alignSelf: 'flex-start',
       }}
-      buttonTextStyle={{...fonts.PrimaryText}}
+      buttonTextStyle={{...fonts.PrimaryText, textAlign: 'left'}}
       defaultButtonText={titleDropdown}
+      renderDropdownIcon={() => {
+        return (
+          <UiIcon iconName={'arrowDown'} iconColor={'black'} iconSize={7} />
+        );
+      }}
+      rowStyle={{height: height(50)}}
+      rowTextStyle={{...fonts.PrimaryText, textAlign: 'left'}}
     />
   );
 };
--- src/screens/Basket/BasketScreen.jsx
+++ src/screens/Basket/BasketScreen.jsx
@@ -3,10 +3,12 @@ import {View, Text} from 'react-native';
 import {UiCounter, UiDropdown} from '../../components/ui-kit';
 
 const BasketScreen = () => {
+  const categories = ['Комплексный обед', 'Салаты', 'Супы', 'Горячее из мяса'];
+
   return (
     <View>
       <UiCounter value={1} isVertical={true} />
-      <UiDropdown titleDropdown={'Комплексный обед'} />
+      <UiDropdown titleDropdown={'Категория'} categories={categories} />
     </View>
   );
 };
