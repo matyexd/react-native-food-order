44de7b217c696662d6b0dade743fb5c1ee6a7422
Darya Shvetsova <danshvetsova@gmail.com>
2022-05-09 11:16:43 +0700

    Функционал dropdown

--- src/components/ui-kit/UiDropdown/UiDropdown/UiDropdown.jsx
+++ src/components/ui-kit/UiDropdown/UiDropdown/UiDropdown.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SelectDropdown from 'react-native-select-dropdown';
 // import {View} from 'react-native'
 import {fonts} from '../../../../theme/Fonts';
 import UiIcon from '../../UiIcon';
 import {styles} from './UiDropdownStyle';
 
-const UiDropdown = ({titleDropdown = 'Select', items}) => {
+const UiDropdown = ({titleDropdown = 'Select', items, filter}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <SelectDropdown
+      onFocus={() => setIsOpen(true)}
+      onBlur={() => setIsOpen(false)}
       data={items}
       onSelect={(selectedItem, index) => {
+        filter(selectedItem);
         console.log(selectedItem);
       }}
       buttonTextAfterSelection={(selectedItem, index) => {
@@ -22,13 +27,23 @@ const UiDropdown = ({titleDropdown = 'Select', items}) => {
       buttonTextStyle={styles.buttonDropdownTextStyle}
       defaultButtonText={titleDropdown}
       renderDropdownIcon={() => {
-        return (
+        return isOpen ? (
+          <UiIcon
+            iconName={'arrowDown'}
+            iconColor={'black'}
+            iconSize={7}
+            style={{
+              transform: [{rotate: '180deg'}],
+            }}
+          />
+        ) : (
           <UiIcon iconName={'arrowDown'} iconColor={'black'} iconSize={7} />
         );
       }}
       rowStyle={styles.rowDropdownStyle}
       rowTextStyle={styles.rowDropdownTextStyle}
       dropdownStyle={styles.dropdownStyle}
+      dropdownOverlayColor="#ffffff00" //Убирает затемнение при открытии
     />
   );
 };
